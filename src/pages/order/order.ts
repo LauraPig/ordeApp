6d648c644e73e48792e292b2fd04af21973bb193
Jimmy Liang <jianning.liang@symbio.com>
2018-06-07 18:01:05 +0800

    6-7-2

--- src/pages/order/order.ts
+++ src/pages/order/order.ts
@@ -5,6 +5,7 @@ import * as moment from 'moment'
 import {DataBaseService} from "../../providers/database/database";
 import {SQLiteObject} from "@ionic-native/sqlite";
 import {SelectTypePage} from "../select-type/select-type";
+import { Storage } from '@ionic/storage';
 
 /**
  * Generated class for the OrderPage page.
@@ -40,12 +41,27 @@ export class OrderPage {
     public navCtrl: NavController,
     public navParams: NavParams,
     public modalCtrl: ModalController,
+    public storage: Storage,
     public dbService: DataBaseService,
   ) {
 
     // 获取参数
     this.factoryId = this.navParams.get('factoryId');
     this.factoryName = this.navParams.get('factoryName');
+
+    // 获取工厂ID
+    this.storage.get('factoryId').then(res =>{
+      if (res) {
+        this.factoryId = res;
+      }
+    });
+
+    // 获取工厂名称
+    this.storage.get('factoryName').then(res =>{
+      if (res) {
+        this.factoryName = res;
+      }
+    });
 
 
     this.monStr = (new Date().getMonth() + 1).toString();
@@ -94,6 +110,8 @@ export class OrderPage {
                 label: res.rows.item(i).label,
                 imgUrl: `assets/imgs/${res.rows.item(i).value}.png`,
                 value: res.rows.item(i).value,
+                officeList: [],
+                status: false,
               });
 
 
@@ -118,6 +136,7 @@ export class OrderPage {
     // listSelector.children
   }
 
+  // 日期选择事件
   onChange($event) {
     console.log($event);
     const result = moment().format('YYYY-MM-DD');
@@ -130,6 +149,49 @@ export class OrderPage {
     this.selectDay = moment($event).format('YYYY年MM月DD');
     // this.getGoalDay();
   }
+
+
+  // 获取餐别下的餐厅
+  getMealTypeDetail(item: any, index: number) {
+    // alert('index---->' + index);
+    // alert('item---->' + item.value);
+    this.typeList[index].status = item.status === false;
+
+    let name: string = '';
+    let temList: Array<any> = [];
+    if (item.value && this.factoryId) {
+      // alert('value' + this.value);
+      // alert('factoryId' + this.factoryId);
+      this.dbService.openDataBase().then((db: SQLiteObject) =>{
+        db.executeSql(`select DISTINCT  a.name, a.id from sys_office a ,ct_meal b where  a.parent_ids LIKE '%${this.factoryId}%' AND a.type='4' and b.office_id = a.id and b.meal_type = '${item.value}' AND b.del_flag='0' AND a.del_flag='0';`,{}).then(res =>{
+          // alert('res: ' + res.rows.length);
+          if (res.rows.length) {
+            for (let i =0; i < res.rows.length; i ++ ) {
+              name = res.rows.item(i).name;
+              temList.push({
+                id: res.rows.item(i).id,
+                name,
+                imgUrl: 'assets/imgs/4.png'
+              });
+            }
+
+            // alert('temList: ' + temList);
+            this.typeList[index].officeList = temList;
+            // alert('result: ' + this.typeObj[`${this.value}`]);
+          }
+        }).catch(e =>{
+          console.log(e);
+        })
+      }).catch(e =>{
+        console.log(e);
+      });
+    }
+  }
+
+  // getPageData() {
+  //
+  // }
+
 
   gotoSelectTypePage(value: string, factoryName: string) {
     // alert('this.monstr--order: ' + this.monStr);
